8967d6852026f35d0f1a927bf9aeada6d0e8f553
Jeff Mitchell <sentinel1909@protonmail.com>
2023-02-22 07:06:25 -0800

    Completed implementation of health_check endpoint

---
+++ progress.md
@@ -0,0 +1,1 @@
+# Progress to Date
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+// main.rs
+
+// dependcencies
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use color_eyre::eyre::Result;
+use std::net::SocketAddr;
+
+// health_check handler
+async fn health_check() -> impl IntoResponse {
+    (StatusCode::OK, Html("<h1>Alive, 200 OK</h1>"))
+}
+
+// main function
+#[tokio::main]
+async fn main() -> Result<()> {
+    // initialize color_eyre for nice looking error messages
+    color_eyre::install()?;
+
+    // routes and their corresponding handlers
+    let app = Router::new().route("/health_check", get(health_check));
+
+    // spin up and listen on part 127.0.0.1:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
 }
