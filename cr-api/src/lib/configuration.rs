04091e97091ea6b12076668d27bff1187f615d0d
Jeff Mitchell <sentinel1909@protonmail.com>
2023-02-26 16:48:06 -0800

    Begin implementation of testing for dbase

--- cr-api/src/lib/configuration.rs
+++ cr-api/src/lib/configuration.rs
@@ -19,6 +19,15 @@ pub struct DatabaseSettings {
   pub database_name: String,
 }
 
+impl DatabaseSettings {
+  pub fn connection_string(&self) -> String {
+    format!(
+      "postgres://{}:{}@{}:{}/{}",
+      self.username, self.password, self.host, self.port, self.database_name
+    )
+  }
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
   // Initialize our configuration reader
   let settings = config::Config::builder()
--- cr-api/tests/health_check.rs
+++ cr-api/tests/health_check.rs
@@ -1,5 +1,7 @@
 //! tests/health_check.rs
 
+use sqlx::{PgConnection, Connection};
+use cr_api::configuration::get_configuration;
 use std::net::TcpListener;
 
 /// Spin up an instance of our application
@@ -34,6 +36,11 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
     let client = reqwest::Client::new();
 
     // Act
@@ -48,6 +55,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
