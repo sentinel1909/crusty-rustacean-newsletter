2fca38eb6bbcdff4ceb6dceeb898220fd4961758
Jeff Mitchell <sentinel1909@protonmail.com>
2023-02-28 20:42:57 -0800

    Turn off dead code warnings for now

--- cr-api/src/lib/routes/subscriptions.rs
+++ cr-api/src/lib/routes/subscriptions.rs
@@ -7,15 +7,16 @@ use axum::{
 
 use serde::Deserialize;
 
-// data structure to model the incoming form data from the subscribe route
+// data structure to model the incoming form data from the subscribe route, will remove dead_code annotation in the future
 #[derive(Deserialize)]
+#[allow(dead_code)]
 pub struct SubscriptionData {
     email: String,
     name: String
 }
 
-// subscriptions handler
-pub async fn subscribe(Form(subcription_data): Form<SubscriptionData>) -> StatusCode {
+// subscriptions handler, for now the form paramater is not used and is marked as such
+pub async fn subscribe(Form(_subcription_data): Form<SubscriptionData>) -> StatusCode {
     StatusCode::OK
 }
 
