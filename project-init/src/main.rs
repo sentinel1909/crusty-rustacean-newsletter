e72216db2cc73365dea283ccca22558044ce361e
Jeff Mitchell <sentinel1909@protonmail.com>
2023-03-08 06:57:35 -0800

    1st attempt to update Shuttle with database

--- project-init/src/main.rs
+++ project-init/src/main.rs
@@ -3,8 +3,11 @@
 use cmd_lib::*;
 
 fn main() -> CmdResult {
-    let msg = "This is the project init crate...";
-    run_cmd!(echo $msg)?;
-
+    let init_msg = "Initializing database...";
+    run_cmd!(echo $init_msg)?;
+    run_cmd!(./cr-api/scripts/init_db.sh)?;
+    let test_msg = "Running tests...";
+    run_cmd!(echo $test_msg)?;
+    run_cmd!(cargo test)?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,32 @@
 //! lib.rs for shuttle deployment
 
 // dependencies
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use shuttle_service::error::CustomError;
+use sqlx::{Executor, PgPool};
 use sync_wrapper::SyncWrapper;
 
 // pull in the routes from the non-shuttle side of the app
 use cr_api::routes::health_check::health_check;
+use cr_api::routes::subscriptions::subscribe;
 
 // shuttle specific startup function
 #[shuttle_service::main]
-async fn axum() -> shuttle_service::ShuttleAxum {
-    let router = Router::new().route("/health_check", get(health_check));
+async fn axum(
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+) -> shuttle_service::ShuttleAxum {
+    pool.execute(include_str!("../schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
+    
+    let router = Router::new()
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe))
+        .with_state(pool);
+    
     let sync_wrapper = SyncWrapper::new(router);
 
     Ok(sync_wrapper)
