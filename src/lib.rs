7591c13962a75d7564adaaaf3dd86a26f9abb217
Jeff Mitchell <sentinel1909@protonmail.com>
2023-03-08 20:34:49 -0800

    Continue to refactor to add database

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,17 @@
 
 // dependencies
 use axum::{routing::get, Router};
+use shuttle_service::error::CustomError;
 use sqlx::PgPool;
 use sync_wrapper::SyncWrapper;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+// pull in routes from the cr_api_local crate
+use cr_api_local::routes::health_check::health_check;
 
 // shuttle specific startup function
 #[shuttle_service::main]
 async fn axum(#[shuttle_shared_db::Postgres] _pool: PgPool) -> shuttle_service::ShuttleAxum {
-    let router = Router::new().route("/hello", get(hello_world));
+    let router = Router::new().route("/health_check", get(health_check));
 
     let sync_wrapper = SyncWrapper::new(router);
 
