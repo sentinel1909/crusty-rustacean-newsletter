b7ed6683a19c233adcb3d2d82f83212fe7d98bd3
Jeff Mitchell <sentinel1909@protonmail.com>
2023-03-08 19:45:52 -0800

    Modify Shuttle Axum code to add database

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 
 // dependencies
 use axum::{routing::get, Router};
+use sqlx::PgPool;
 use sync_wrapper::SyncWrapper;
 
 async fn hello_world() -> &'static str {
@@ -10,7 +11,7 @@ async fn hello_world() -> &'static str {
 
 // shuttle specific startup function
 #[shuttle_service::main]
-async fn axum() -> shuttle_service::ShuttleAxum {
+async fn axum(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_service::ShuttleAxum {
     let router = Router::new().route("/hello", get(hello_world));
 
     let sync_wrapper = SyncWrapper::new(router);
