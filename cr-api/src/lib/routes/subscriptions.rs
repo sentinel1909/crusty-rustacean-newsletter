8ba22db90255f15751d0e2a51b7bfabbe18e7d5d
Jeff Mitchell <sentinel1909@protonmail.com>
2023-04-09 21:07:55 -0700

    Add status column to subscriptions table, adjust subscriptions query to include status

--- cr-api/src/lib/routes/subscriptions.rs
+++ cr-api/src/lib/routes/subscriptions.rs
@@ -28,7 +28,7 @@ pub async fn insert_subscriber(
 ) -> Result<(), sqlx::Error> {
     let _ = sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
+        INSERT INTO subscriptions (id, email, name, subscribed_at, status) VALUES ($1, $2, $3, $4, 'confirmed')
         "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
