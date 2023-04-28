0288ddbb6d859222b3c94dac9e2bfbe6d250f558
Jeff Mitchell <sentinel1909@protonmail.com>
2023-04-27 19:09:39 -0700

    Run cargo fmt before commit

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@
 
 // dependencies
 use anyhow::anyhow;
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
 use cr_api_docker::routes::health_check;
 use shuttle_runtime::CustomError;
 use shuttle_secrets::SecretStore;
@@ -14,9 +11,9 @@ use sqlx::{Executor, PgPool};
 // shuttle runtime main function
 #[shuttle_runtime::main]
 async fn axum(
-    #[shuttle_shared_db::Postgres] pool: PgPool, #[shuttle_secrets::Secrets] secret_store: SecretStore,
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> shuttle_axum::ShuttleAxum {
-
     // load secrets from Secrets.toml (production)
     let _secret = if let Some(secret) = secret_store.get("BASE_URL") {
         secret
@@ -32,8 +29,7 @@ async fn axum(
     .map_err(CustomError::new)?;
 
     // spin up the router
-    let router = Router::new()
-        .route("/health_check", get(health_check));
+    let router = Router::new().route("/health_check", get(health_check));
 
     // return the router
     Ok(router.into())
