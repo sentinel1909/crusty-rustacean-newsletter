60343d7ff879a45e3e84fd5a71650a8cb3bde729
Jeff Mitchell <sentinel1909@protonmail.com>
2023-04-29 10:43:20 -0700

    Misc re-factoring

--- cr-api/src/lib/configuration.rs
+++ cr-api/src/lib/configuration.rs
@@ -1,7 +1,6 @@
 //! src/lib/configuration.rs
 
-use std::u16;
-
+// dependencies, internal and external
 use crate::domain::SubscriberEmail;
 use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
@@ -9,7 +8,9 @@ use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::PgConnectOptions;
 use sqlx::postgres::PgSslMode;
 use sqlx::ConnectOptions;
+use std::u16;
 
+// a struct to hold a type for settings
 #[derive(Clone, Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
@@ -17,6 +18,7 @@ pub struct Settings {
     pub email_client: EmailClientSettings,
 }
 
+// a struct to hold a type for application settings
 #[derive(Clone, Deserialize)]
 pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
@@ -25,6 +27,7 @@ pub struct ApplicationSettings {
     pub base_url: String,
 }
 
+// a struct to hold a type for email client settings
 #[derive(Clone, Deserialize)]
 pub struct EmailClientSettings {
     pub base_url: String,
@@ -33,6 +36,7 @@ pub struct EmailClientSettings {
     pub timeout_milliseconds: u64,
 }
 
+// implement sender and timeout functions for Email Client
 impl EmailClientSettings {
     pub fn sender(&self) -> Result<SubscriberEmail, String> {
         SubscriberEmail::parse(self.sender_email.clone())
@@ -43,6 +47,7 @@ impl EmailClientSettings {
     }
 }
 
+// a struct to hold a type for database settings
 #[derive(Clone, Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
@@ -54,6 +59,7 @@ pub struct DatabaseSettings {
     pub require_ssl: bool,
 }
 
+// implementation for functions to return database connection options
 impl DatabaseSettings {
     pub fn with_db(&self) -> PgConnectOptions {
         let mut options = self.without_db().database(&self.database_name);
@@ -76,6 +82,39 @@ impl DatabaseSettings {
     }
 }
 
+// an enum to hold app environment options, be it local or production
+pub enum Environment {
+    Local,
+    Production,
+}
+
+// implementation to return a string with values for either local or production
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+// implementation to convert environment string names into the Environment enum
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. \
+Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
+}
+
+// function to read in values from the configuration files
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
@@ -100,31 +139,4 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         )
         .build()?;
     settings.try_deserialize::<Settings>()
-}
-
-pub enum Environment {
-    Local,
-    Production,
-}
-impl Environment {
-    pub fn as_str(&self) -> &'static str {
-        match self {
-            Environment::Local => "local",
-            Environment::Production => "production",
-        }
-    }
-}
-impl TryFrom<String> for Environment {
-    type Error = String;
-    fn try_from(s: String) -> Result<Self, Self::Error> {
-        match s.to_lowercase().as_str() {
-            "local" => Ok(Self::Local),
-            "production" => Ok(Self::Production),
-            other => Err(format!(
-                "{} is not a supported environment. \
-Use either `local` or `production`.",
-                other
-            )),
-        }
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,18 @@
 // main.rs for Shuttle deployment
 
 // dependencies
-use anyhow::anyhow;
 use axum::{routing::get, Router};
-use cr_api_docker::routes::health_check;
-use shuttle_runtime::CustomError;
-use shuttle_secrets::SecretStore;
-use sqlx::{Executor, PgPool};
 
-// shuttle runtime main function
+// hello handler, returns a simple message
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+// main function, annoted with the Shuttle runtime macro
 #[shuttle_runtime::main]
-async fn axum(
-    #[shuttle_shared_db::Postgres] pool: PgPool,
-    #[shuttle_secrets::Secrets] secret_store: SecretStore,
-) -> shuttle_axum::ShuttleAxum {
-    // load secrets from Secrets.toml (production)
-    let _secret = if let Some(secret) = secret_store.get("BASE_URL") {
-        secret
-    } else {
-        return Err(anyhow!("Secret was not found.").into());
-    };
-
-    // set up the database pool and perform migrations
-    pool.execute(include_str!(
-        "../cr-api/migrations/20230225210742_create_subscriptions_table.sql"
-    ))
-    .await
-    .map_err(CustomError::new)?;
-
-    // spin up the router
-    let router = Router::new().route("/health_check", get(health_check));
+async fn axum() -> shuttle_axum::ShuttleAxum {
+    // create a router, with a /hello endpoint
+    let router = Router::new().route("/hello", get(hello_world));
 
     // return the router
     Ok(router.into())
