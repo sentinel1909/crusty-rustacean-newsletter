9183666fad3baca487ab76c889808741e3b37d4d
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-06-30 14:07:54 -0700

    Remove extraneous comments from /newsletter/post

--- cr-api/src/lib/routes/admin/newsletter/post.rs
+++ cr-api/src/lib/routes/admin/newsletter/post.rs
@@ -88,11 +88,7 @@ pub async fn publish_newsletter(
             }
             Err(error) => {
                 tracing::warn!(
-                // We record the error chain as a structured field
-                // on the log record.
                 error.cause_chain = ?error,
-                // Using `\` to split a long string literal over
-                // two lines, without creating a `\n` character.
                 "Skipping a confirmed subscriber. \
                 Their stored contact details are invalid",
                 );
